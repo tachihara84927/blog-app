36f2cc8a0e98c32399df8dcc11e40b2341bea1a5
nekoGorilla <for.kaihatsu.168@gmail.com>
2020-05-11 00:07:45 +0900

    forms and accounts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+    )
 
 urlpatterns = [
+    # home
     path("", BlogListView.as_view(), name="home"),
+    # detail
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    # post new
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    # post edit
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    # post delete
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
 ]
